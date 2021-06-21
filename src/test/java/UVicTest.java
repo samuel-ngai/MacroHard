b7368b78930149e71a2b33d0d4e5346e0d16c173
hsmethurst <hsmethurst@uvic.ca>
2021-06-21 12:55:40 -0700

    Added tests for uvic's webpage.

--- src/test/java/UVicTest.java
+++ src/test/java/UVicTest.java
@@ -22,7 +22,7 @@ public class UVicTest {
     @BeforeEach
     public void setUp() {
         // Chrome
-        System.setProperty("webdriver.chrome.driver", "*****LOCATION OF YOUR WEBDRIVER*****");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         browser = new ChromeDriver();
 
         // Firefox
@@ -35,12 +35,56 @@ public class UVicTest {
         browser.manage().window().maximize();
     }
 
+    //new WebDriverWait(browser,5).until(ExpectedConditions.titleIs("uvic -Google Search"));
+
     @AfterEach
     public void cleanUp() {
         browser.quit();
     }
 
+    public WebElement validifyById(String id) {
+        WebElement element = browser.findElement(By.id(id));
+        assertNotNull(element);
+        return element;
+    }
 
-    // Your tests go here
+
+    @Test
+    public void googleSearchResultsAppear() {
+        browser.get("https://www.google.com");
+        WebElement inputBox = browser.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        WebElement searchButton = browser.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"));
+        inputBox.sendKeys("uvic");
+        searchButton.click();
+        new WebDriverWait(browser, 5).until(ExpectedConditions.titleIs("uvic - Google Search"));
+        assertEquals("uvic - Google Search", browser.getTitle());
+    }
+
+    @Test
+    public void pageTitle() {
+        browser.get("https://www.uvic.ca");
+        assertEquals("Home - University of Victoria", browser.getTitle()); // task 1
+
+        WebElement phoneNumber = browser.findElement(By.linkText("1-250-721-7211"));
+        assertNotNull(phoneNumber); // task 6
+    }
+
+    @Test
+    public void useSearchBar() {
+        browser.get("https://www.uvic.ca");
+
+        new WebDriverWait(browser,5).until(ExpectedConditions.titleIs("Home - University of Victoria"));
+
+        WebElement searchBtn = validifyById("search-btn"); // task 2
+
+        searchBtn.click();
+        WebElement searchBar = validifyById("searchUVic"); // task 3
+
+        searchBar.sendKeys("csc");
+        assertEquals("csc", searchBar.getAttribute("value")); // task 4
+
+        searchBar.sendKeys(Keys.ENTER);
+        assertEquals("Search - University of Victoria", browser.getTitle()); // task 5
+    }
 
 }
