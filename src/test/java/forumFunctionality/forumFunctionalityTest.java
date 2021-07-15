e8d6f94ed5a889e46ad361e22438c6f791984df7
hsmethurst <hsmethurst@uvic.ca>
2021-07-15 13:27:17 -0700

    Added new tests for Forum Functionality.

--- src/test/java/forumFunctionality/forumFunctionalityTest.java
+++ src/test/java/forumFunctionality/forumFunctionalityTest.java
@@ -3,12 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,8 +15,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class forumFunctionalityTest {
 
     WebDriver browser;
-
     String postText = "Testing Create Post";
+    String replyText = "Testing Create Reply";
 
     @BeforeEach
     public void setUp() {
@@ -28,7 +24,6 @@ public class forumFunctionalityTest {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
-
         browser.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
         browser.findElement(By.id("txtUsername")).sendKeys("Admin");
         browser.findElement(By.id("txtPassword")).sendKeys("admin123");
@@ -45,7 +40,38 @@ public class forumFunctionalityTest {
     public void CreateAPost(){
         createPost(postText);
         waitForPage();
-        assertEquals("Testing Create Post", browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]/div[@id=\"postBodySecondRow\"]/div")).getText());
+        assertEquals(postText, browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]/div[@id=\"postBodySecondRow\"]/div")).getText());
+        deletePost();
+    }
+
+    @Test
+    public void CreateAReply(){
+        createPost(postText);
+        waitForPage();
+        createReply(replyText);
+        assertEquals(replyText, browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]//*[@class='commentContent']")).getText());
+        deletePost();
+    }
+
+    @Test
+    public void LikeAPost(){
+        createPost(postText);
+        waitForPage();
+        likePost();
+        waitForPage();
+        assertEquals("1", browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]//div[@id='noOfLikesLinknew']/a/span")).getText());
+        deletePost();
+    }
+
+    // Fails as the functionality is wrong
+    @Test
+    public void UnlikeAPost(){
+        createPost(postText);
+        waitForPage();
+        likePost();
+        waitForPage();
+        unlikePost();
+        assertEquals("0", browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]//div[@id='noOfLikesLinknew']/a/span")).getText());
         deletePost();
     }
 
@@ -77,11 +103,31 @@ public class forumFunctionalityTest {
         browser.findElement(By.id("postSubmitBtn")).click();
     }
 
+    public void createReply(String text) {
+        waitForPage();
+        WebElement textbox = browser.findElement(By.xpath("//*[@name='createComment[comment]'][@class='commentBox']"));
+
+        textbox.sendKeys(text);
+
+        //alternate:
+        //JavascriptExecutor executor = (JavascriptExecutor)browser;
+        //executor.executeScript("arguments[0].value = 'Testing Create Reply';", textbox);
+        browser.findElement(By.xpath("//*[@class='commentSubmitBtn submitBtn']")).click();
+    }
+
     public void deletePost() {
         browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]/div[@id=\"postBodyFirstRow\"]/div[@id=\"postFirstRowColumnThree\"]/div")).click();
         waitForPage();
         browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]/div[@id=\"postBodyFirstRow\"]/div[@id=\"postFirstRowColumnThree\"]/div/div/div/ul//*[@class=\"deleteShare\"]")).click();
         browser.findElement(By.id("delete_confirm")).click();
+    }
+
+    public void likePost() {
+        browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]//*[@class='likeLinknew']")).click();
+    }
+
+    public void unlikePost() {
+        browser.findElement(By.xpath("//*[@id=\"buzz\"]/li/div[@id=\"postBody\"]//*[@class='likeLinknew']")).click();
     }
 
 
